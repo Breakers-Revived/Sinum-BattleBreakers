f227b337455ebb624c7e7d4bb57d51ca50269f55
Chaikew <68963282+Chaikew@users.noreply.github.com>
2024-11-02 14:11:22 +0100

    Early IOS support for dynamic URLs

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,9 +3,45 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
-#define EPIC_GAMES_URL @"ol.epicgames.com"
+NSString* gApiUrl = @"http://127.0.0.1";
 
+//
+//  DYNAMIC PART
+//
+(BOOL) loadConfigFile() {
+    NSString* configPath = [[NSBundle mainBundle] pathForResource:@"sinum_cfg" ofType:@"txt"];
+    if (!configPath) {
+        NSLog(@"sinum_cfg.txt not found in bundle.");
+        return false;
+    }
+    NSError* error = nil;
+    NSString* configContents = [NSString stringWithContentsOfFile:configPath encoding:NSUTF8StringEncoding error:&error];
+    if (error) {
+        NSLog(@"Failed to load sinum config file: %@", error);
+        return false;
+    }
+
+    if (!configContents) {
+        NSLog(@"Failed to load sinum_cfg.txt. Using default API_URL: %@", gApiUrl);
+        return false;
+    }
+
+    // Trim and remove newline characters from the configuration content
+    NSString* trimmedConfig = [[configContents stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]
+                                stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+    trimmedConfig = [trimmedConfig stringByReplacingOccurrencesOfString:@"\r" withString:@""];
+
+    // Set API_URL dynamically
+    gApiUrl = trimmedConfig;
+    NSLog(@"Loaded and set API_URL from config: %@", gApiUrl);
+    return true;
+}
+
+#define API_URL gApiUrl
+
+//
+//  Actual Sinum hook 
+//
 @interface CustomURLProtocol : NSURLProtocol
 @end
 
@@ -14,13 +50,10 @@
 + (BOOL)canInitWithRequest:(NSURLRequest *)request 
 {  
     NSString *absoluteURLString = [[request URL] absoluteString];
-    if ([absoluteURLString containsString:EPIC_GAMES_URL] && ![absoluteURLString containsString:@"/CloudDir/"]) {
-        if ([NSURLProtocol propertyForKey:@"Handled" inRequest:request]) {
-            return NO;
-        }
-        return YES;
+    if ([NSURLProtocol propertyForKey:@"Handled" inRequest:request]) {
+        return NO;
     }
-    return NO;
+    return YES;
 }
 
 + (NSURLRequest*)canonicalRequestForRequest:(NSURLRequest*)request
@@ -61,6 +94,7 @@
 @end
 
 __attribute__((constructor)) void entry()
-{
+{    
+    loadConfigFile();
     [NSURLProtocol registerClass:[CustomURLProtocol class]];
 }
